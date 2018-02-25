e365a10640e8e7791d9c63757f6197b8e0f16ef8
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-02-24 23:03:03 -0700

    Refactor tests and add setup and clean up methods

--- test/com/developersdelicias/katas/junit/HelloWorldTest.java
+++ test/com/developersdelicias/katas/junit/HelloWorldTest.java
@@ -1,26 +1,44 @@
 package com.developersdelicias.katas.junit;
 
-import org.junit.Test;
+import org.junit.*;
 
 import static org.junit.Assert.assertEquals;
 
 public class HelloWorldTest {
+	private HelloWorld helloWorld;
+
+	@BeforeClass
+	public static void setUpOnce() {
+		System.out.println("*** Setup once for all tests");
+	}
+	@Before
+	public void setUp() {
+		System.out.println("+++ Setup before each test");
+		helloWorld = new HelloWorld();
+	}
 
 	@Test
 	public void can_say_hello_to_a_given_name() {
-		HelloWorld helloWorld = new HelloWorld();
-
+		System.out.println("Testing normal names scenarios");
 		assertEquals("Hello Benjamin!", helloWorld.sayHello("Benjamin"));
 		assertEquals("Hello Alonso!", helloWorld.sayHello("Alonso"));
 	}
 
-
 	@Test
 	public void can_say_hello_to_strangers() {
-		HelloWorld helloWorld = new HelloWorld();
-
+		System.out.println("Testing empty names scenarios");
 		assertEquals("Hello Stranger!", helloWorld.sayHello(null));
 		assertEquals("Hello Stranger!", helloWorld.sayHello(""));
 		assertEquals("Hello Stranger!", helloWorld.sayHello("     "));
+	}
+
+	@After
+	public void tearDown() {
+		System.out.println("--- Tear down after each test");
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+		System.out.println("@@@ Tear down all tests");
 	}
 }
