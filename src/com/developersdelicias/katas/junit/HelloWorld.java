7c135c0fb217e8f848c62b9ce637260a41cbc103
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2018-02-24 21:23:45 -0700

    Adding main to test sayHello method

--- src/com/developersdelicias/katas/junit/HelloWorld.java
+++ src/com/developersdelicias/katas/junit/HelloWorld.java
@@ -5,4 +5,14 @@ public class HelloWorld {
 	public String sayHello(String name) {
 		return "Hello " + name + "!";
 	}
+
+	public static void main(String[] args) {
+		HelloWorld helloWorld = new HelloWorld();
+
+		String name = "Benjamin";
+		String result = helloWorld.sayHello(name);
+
+		System.out.println(result);
+		System.out.println(helloWorld.sayHello("Alonso"));
+	}
 }
